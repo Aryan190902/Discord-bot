d446be00d8c52828f8908053febfa8ee3fa82fad
Aryan apte <b20186@students.iitmandi.ac.in>
2021-06-20 10:34:44 +0530

    Added Luck command

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
 import discord
 import os
 import requests
+from discord.ext import commands
+from random import choice
 
-client = discord.Client()
+my_secret = os.environ['BOT_KEY'] #here goes you r own bot token
 
-my_secret = os.environ['TOKEN']
-api_key = os.environ['API']
-url = 'https://maps.googleapis.com/maps/api/streetview?size=600x400'
+bot=commands.Bot(command_prefix='>')
 
-
-@client.event
+@bot.event
 async def on_ready():
-    print('{0.user} is online'.format(client))
+    print(f'{bot.user} is online.')
+
+@bot.event
+async def on_command_error(ctx, error):
+  if isinstance(error, commands.CommandOnCooldown):
+    control = ['Asking again wont change {x} bad luck 😂',
+    'Wait a minute {x}, you will be roasted again 🔥🔥',
+    'abhi cooldown hai, Jal lijiye {x}, thak gye honge 💧']
+    roast = (choice(control)).format(x = ctx.message.author.mention)
+    await ctx.send(roast)
+
+@bot.command(name='hey',help='gives random rick roll Hi!')
+async def rick_roll(ctx):
+    rick_rolls=('Loser','Nerd','Swine','Sucker','Lazy')
+    response='Hi! '+ choice(rick_rolls)
+    await ctx.send(response)
 
 
-@client.event
-async def on_message(mssg):
-    if mssg.author == client.user:
-        return
+@bot.command(name='baseball', help='Plays a game of base ball. Bot selects a random number \{ 1,2,3\}')
+async def bb(ctx):
+    await ctx.send(f'{ctx.author.mention} has started a base ball game choose one from 1,2,3')
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
 
-    if mssg.content.startswith('>hey'):
-        await mssg.channel.send('Hey! looser')
+    strike=0
+    score=0
+    while strike<3:
+        msg = await bot.wait_for("message", check=check)
+        if msg.content in ('1','2','3'):
+            if choice(('1','2','3')) == msg.content:
+                strike+=1
+                await ctx.send(f'{ctx.author.mention} Strike {strike}')
+            else:
+                score+=int(msg.content)
+                await ctx.send(f'{ctx.author.mention} Score {score}')
+        else:
+            break
+    
+    await ctx.send(f'Good Game! {ctx.author.mention} Your score was {score}')
 
-    x = mssg.content.replace('>find ', '')
-    y = x.replace(' ', '+')
-    def findIt():
-        r = requests.get(url + "location=" + y + "&key=" + api_key)
-        print(r.text)
-         
+@bot.command(name='luck', help="Checks how lucky you are")
+@commands.cooldown(1, 60, commands.BucketType.user)
+async def luck(ctx):
+  lst = ['|| You are a piece of shit {x} 🤢 ||', '|| Why {x} is testing their bad luck😂. ||',
+    '|| {x}\'s luck = 💯. ||',
+    '|| Hey! loser {x} 🤣 ||']
+    
+  y = (choice(lst)).format(x=ctx.message.author.mention)
+  await ctx.send(y)
 
-    if mssg.content.startswith(('>find {0}').format(x)):
-        return findIt()
-
-
-client.run(my_secret)
+bot.run(my_secret)
